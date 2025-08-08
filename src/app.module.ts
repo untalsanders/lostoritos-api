5833e0496a791ac0737f67eb5925ac24629a9c6d
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-08 07:13:14 -0300

    feat: integrate ConfigModule for environment variable management
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { PlayersModule } from './players/players.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+    }),
+    PlayersModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
+
+const configService = new ConfigService()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const logger = new Logger('Bootstrap')
+  const host = configService.get('HOST')
+  const port = configService.get('PORT')
+  const prefix = configService.get('CONTEXT_API')
+
+  app.setGlobalPrefix(prefix)
+  await app.listen(port, host)
+  logger.log(`Server is running in ${await app.getUrl()}`)
 }
 bootstrap()
