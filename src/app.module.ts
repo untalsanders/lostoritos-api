7b244a536cf2e7deeadd092377a70852ccc581dc
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-16 18:29:55 -0300

    feat: add global filters and middlewares
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
-import { PlayerModule } from './modules/player/player.module'
 import { ConfigModule } from '@nestjs/config'
+import { APP_FILTER } from '@nestjs/core'
 import { AppController } from './app.controller'
+import { PlayerModule } from './modules/player/player.module'
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter'
 
 @Module({
+  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
@@ -12,6 +15,11 @@ import { AppController } from './app.controller'
     }),
     PlayerModule,
   ],
-  controllers: [AppController]
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
+import { logger } from './shared/middlewares/logger.middleware'
 
 const configService = new ConfigService()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(logger)
   app.enableCors()
   await app.listen(configService.get('HOST') ?? 3000)
 }
