c3223608f9b091575c0561318016ea5c7430de6b
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-16 03:58:22 -0300

    refactor(main): simplify bootstrap function by removing logger and global prefix setup
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
-import { Logger } from '@nestjs/common'
 
 const configService = new ConfigService()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const logger = new Logger('Bootstrap')
-  const host = configService.get('HOST')
-  const port = configService.get('PORT')
-  const prefix = configService.get('CONTEXT_API')
-
-  app.setGlobalPrefix(prefix)
-  await app.listen(port, host)
-  logger.log(`Server is running in ${await app.getUrl()}`)
+  app.enableCors()
+  await app.listen(configService.get('HOST') ?? 3000)
 }
 bootstrap()
