4578a648707b63359b845222960d3ff82dd25839
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-17 20:30:48 -0300

    fix: remove AppController
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { APP_FILTER } from '@nestjs/core'
-import { AppController } from './app.controller'
 import { PlayerModule } from './modules/player/player.module'
-import { HttpExceptionFilter } from './shared/filters/http-exception.filter'
 
 @Module({
-  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
@@ -14,12 +10,6 @@ import { HttpExceptionFilter } from './shared/filters/http-exception.filter'
       expandVariables: true,
     }),
     PlayerModule,
-  ],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
   ],
 })
 export class AppModule {}
