289bcb0656fc211de883d5eacd8bc5ae032e1325
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-17 22:25:33 -0300

    feat: change prefix for logger
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { logger } from './shared/middlewares/logger.middleware'
+import { ConsoleLogger } from '@nestjs/common'
 
 const configService = new ConfigService()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.use(logger)
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'LOS_TORITOS_APP', // Default is "Nest"
+    }),
+  })
   app.enableCors()
   await app.listen(configService.get('PORT') ?? 3000)
 }
